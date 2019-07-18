c721d80b6ed63414eb16bd62928816a27b75e438
Vasudha Subramanian <subramanian.vasudha@gmail.com>
2019-07-18 10:00:49 -0700

    Initial Commit

---
+++ TriviaGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TriviaGame
+//
+//  Created by 23VasudhaSubramanian on 7/18/19.
+//  Copyright © 2019 Vasudha Subramanian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
