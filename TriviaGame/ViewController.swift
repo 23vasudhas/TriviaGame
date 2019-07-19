00da9e10a69f7ac813c9fc28c4bf5c4fdf7fe538
Vasudha Subramanian <subramanian.vasudha@gmail.com>
2019-07-19 10:09:48 -0700

    enter key press = check button complete

--- TriviaGame/ViewController.swift
+++ TriviaGame/ViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
+//variables questions, answers, score, currentQ
     var questions = ["The more you take away, the more I become. What am I?", "I get wet while drying. What am I?", "The more you take the more you leave behind.  What am I?", "I am tall when young and short when I am old. What am I?", "I go up and never come down.  What am I?", "I disappear every time you say my name. What am I?", "I fly without wings.  What am I?", "If you have me, you want to share me. Once you share me, you won’t have me. What am I?", "I start with an E, end with an E, and have a letter in me. What am I?", "I sit in a corner while traveling around the world. What am I?", "I can fall off a building and live, but put me in water I will die. What am I?", "I have a face but no eyes, hands but no arms. What am I?", "I am full of holes but still hold water. What am I?", "The faster you run, the harder it is to catch me. What am I?"]
     var currentQ = 0
-    var answers = ["Hole", "Towel", "Footsteps", "Candle", "Age", "Silence", "Time", "Secret", "Envelope", "Stamp", "Paper", "Clock", "Sponge", "Your breath"]
+    var answers = ["hole", "towel", "footsteps", "candle", "age", "silence", "time", "secret", "envelope", "stamp", "paper", "clock", "sponge", "your breath"]
     var score = 0
     
-    
+//IBOutlet for labels and buttons
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var userAnswerTextField: UITextField!
     @IBOutlet weak var validationLabel: UILabel!
@@ -28,8 +29,8 @@ class ViewController: UIViewController {
         questionLabel.text = questions[currentQ]
         menuButton.isHidden = true
     }
-
-    @IBAction func checkButtonClick(_ sender: Any) {
+    
+    func processAnswer() {
         if(userAnswerTextField.text == ""){
             validationLabel.text = "Please enter an answer."
             return
@@ -43,6 +44,8 @@ class ViewController: UIViewController {
         } else {
             validationLabel.text = "Incorrect. The correct answer is \(correctAnswer)."
         }
+        
+        //end of game messages
         currentQ += 1
         if (currentQ >= questions.count){
             if(score > questions.count / 2 ) {
@@ -58,7 +61,17 @@ class ViewController: UIViewController {
         userAnswerTextField.text = ""
         scoreLabel.text = "Score: \(String(score))"
     }
-    
+
+
+
+
+    @IBAction func textFieldEnterPress(_ sender: Any) {
+        processAnswer()
+    }
+    //responses for when user answers the question
+    @IBAction func checkButtonClick(_ sender: Any) {
+        processAnswer()
+    }
+        
+
 }
-
-
